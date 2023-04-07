d09ceb8cfb493c5f1cad7841226fa805b8bd6fde
mordecaioladimeji <cai.oladimeji@gmail.com>
2023-04-07 11:25:50 +0100

    second half of async fetch function to parse json. On submit event handler for form

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Pet from "./Pet";
 
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 const breeds = [];
@@ -7,9 +8,25 @@ const SearchParams = () => {
   const [location, setLocation] = useState("");
   const [animal, setAnimal] = useState("");
   const [breed, setBreed] = useState("");
+  const [[pets, setPets]] = useState([]);
+
+  useEffect(() => {
+    requestPets()
+  }, []);
+
+  async function requestPets() {
+    const res = await fetch(`http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
+    );
+    const json = await res.json();
+
+    setPets(json.pets);
+  }
   return (
     <div className="search-params">
-      <form>
+      <form onSubmit={(e) => {
+        e.preventDefault;
+        requestPets();
+      }}>
         <label htmlFor="location">
           Location
           <input onChange={(e) => setLocation(e.target.value)} id="location" value={location} placeholder="location" />
@@ -38,8 +55,17 @@ const SearchParams = () => {
             })}
           </select>
         </label>
-        <button>Submit</button>
+        <button >Submit</button>
       </form>
+      {
+        pets.map((pet) => {
+          <Pet name={pet.name}
+            animal={pet.animal}
+            breed={pet.breed}
+            key={pet.id}
+          />
+        })
+      }
     </div>
   );
 };
